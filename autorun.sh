11d4d8917673ad9bb637cf7f35234c8548082cb0
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-02-12 18:19:29 +0100

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -4,8 +4,22 @@ echo $(dirname $0)
 
 python3 -m pip install requests
 
-python3 python code/main.py input/config.json
+python -m pip install --upgrade pip
+          
+pip3 install -r code/requirements.txt
+      - name: execute py script 
+        run: python YouTubeToURL/code/main.py YouTubeToURL/input/config.json
+      - name: commit and push files
+      
+        run: |
+          git config --global user.email "<>"
+          git config --local user.name "master bot"
+          git add -A
+          git commit -m " Update"
+          git push
 
-echo m3u grabbed
+
+
+
 
 
