ef30ff38742762fe831f953b60160368b3f8fd67
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-02-12 18:12:34 +0100

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -6,6 +6,19 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 youtube_m3ugrabber.py > ../youtube.m3u
-
-echo m3u grabbed
+            python-version: '3.x' 
+      - name: Install dependencies
+        run: |
+          python -m pip install --upgrade pip
+          
+          pip3 install -r YouTubeToURL/code/requirements.txt
+      - name: execute py script 
+        run: python code/main.py input/config.json
+      - name: commit and push files
+      
+        run: |
+          git config --global user.email "<>"
+          git config --local user.name "master bot"
+          git add -A
+          git commit -m " Update"
+          git push
